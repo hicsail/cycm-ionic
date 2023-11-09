c1a7e2a22b45c5386dbaf5438b218d1e18e0afdf
Asad <am5815@bu.edu>
2023-11-09 15:57:02 -0500

    first commit

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -9,6 +9,9 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
     <div id="container">
       <strong>{name}</strong>
       <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 };
