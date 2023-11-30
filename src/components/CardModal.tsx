bf295d54396f463e537f30fa4146c1e5bb8a3c06
Asad <am5815@bu.edu>
2023-11-30 11:49:27 -0500

    removed commented code

--- src/components/CardModal.tsx
+++ src/components/CardModal.tsx
@@ -19,7 +19,6 @@ import {
 import { Share } from '@capacitor/share';
 import { share, shareOutline, playCircle, add, closeCircle, pauseCircle } from 'ionicons/icons';
 
-
 interface Props {
   title: string;
   sentences: string[];
@@ -30,7 +29,6 @@ interface Props {
 
 const CardModal: React.FC<Props> = ({ title, sentences, id, voiceId, manual_id }) => {
 
-  const [modalOpen, setModalOpen] = useState<boolean>(false);
   const modal = useRef<any>(null);
   const input = useRef<any>(null);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -40,32 +38,6 @@ const CardModal: React.FC<Props> = ({ title, sentences, id, voiceId, manual_id }
   const [hasFetchedAudios, setHasFetchedAudios] = useState(false);
   const sentenceRefs = useRef<(HTMLIonTextElement | null)[]>([]);
   const [displayVideo, setDisplayVideo] = useState<boolean>(true);
-
-  // const fetchAudio = async (sentence: string) => {
-  //   // ... existing fetchAudio code ...
-  //   const apiKey = process.env.NEXT_PUBLIC_ELEVEN_LABS_API_KEY;
-
-  //   if (!apiKey) {
-  //     throw new Error('ELEVEN_LABS_API_KEY is not defined');
-  //   }
-
-  //   try {
-  //     const response = await fetch(`https://cycm.s3.amazonaws.com/article_audios/article_${manual_id}/${voiceId}/audio_${index + 1}.mp3`);
-
-  //     if (!response.ok) {
-  //       const errorData = await response.json();
-  //       console.error('Error response from server:', errorData);
-  //       return null;
-  //     }
-
-  //     const blob = await response.blob();
-  //     const url = URL.createObjectURL(blob);
-  //     return new Audio(url);
-  //   } catch (error) {
-  //     console.error('Error occurred while making request:', error);
-  //     return null;
-  //   }
-  // };
 
   const fetchAudio = async (index: number) => {
     try {
@@ -110,22 +82,6 @@ const CardModal: React.FC<Props> = ({ title, sentences, id, voiceId, manual_id }
     if (isPlaying && !hasFetchedAudios) fetchAudios();
   }, [sentences, isPlaying]);
 
-  // Modify the useEffect hook that handles the isPlaying state variable
-  // useEffect(() => {
-  //   if (isPlaying && currentIndex < sentences.length) {
-  //     const currentAudio = audios[currentIndex];
-  //     if (currentAudio && isPlaying) {
-  //       currentAudio.onended = () => {
-  //         setCurrentIndex(prevIndex => prevIndex + 1);
-  //       };
-  //       currentAudio.play();
-  //     }
-
-  //   } else if (currentIndex === sentences.length) {
-  //     setIsPlaying(false); // Set isPlaying to false when the last sentence has been played
-  //   }
-  // }, [isPlaying, currentIndex, sentences.length, audios]);
-
   useEffect(() => {
     if (isPlaying && currentIndex < sentences.length) {
       const currentAudio = audios[currentIndex];
@@ -144,25 +100,6 @@ const CardModal: React.FC<Props> = ({ title, sentences, id, voiceId, manual_id }
       }
     }
   }, [isPlaying, currentIndex, sentences.length, audios]);
-
-  // useEffect(() => {
-  //   if (currentIndex > 0) {
-  //     const container: any = containerRef.current;
-  //     setTimeout(() => {
-  //       const highlightedElement = container.querySelector('.highlight');
-  //       if (highlightedElement) {
-  //         const containerHeight = container.clientHeight;
-  //         const highlightedTop = highlightedElement.offsetTop;
-  //         const highlightedHeight = highlightedElement.offsetHeight;
-  //         const scrollTo = highlightedTop - containerHeight / 2 + highlightedHeight / 2;
-  //         container.scrollTo({
-  //           top: scrollTo,
-  //           behavior: 'smooth',
-  //         });
-  //       }
-  //     }, 0);
-  //   }
-  // }, [currentIndex]);
 
   useEffect(() => {
     const currentSentenceRef = sentenceRefs.current[currentIndex];
@@ -231,22 +168,6 @@ const CardModal: React.FC<Props> = ({ title, sentences, id, voiceId, manual_id }
           </IonToolbar>
         </IonHeader>
         <IonContent color={'dark'}>
-          {/* <div ref={containerRef} style={{
-            maxHeight: '100vh',
-          }}>
-            <video src='./calm-video1.mp4' autoPlay loop muted />
-            {sentences.map((sentence, index) => (
-              <IonText style={{
-                display: 'block',
-                padding: '1rem',
-                fontSize: '1.5rem',
-                lineHeight: '2rem',
-              }} className={index == currentIndex ? 'highlight' : ''} color={index <= currentIndex ? "success" : "medium"} key={index}>
-                <h3>{sentence}.</h3>
-              </IonText>
-            ))}
-            
-          </div> */}
           <div ref={containerRef} style={{
             position: 'relative',
             height: '100vh',
