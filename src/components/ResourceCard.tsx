1616ee3ce03690f099a5214b0da92da5ca19da0b
wenhwang97 <wenhwang@bu.edu>
2023-12-08 15:23:24 -0500

    fix cards

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -1,9 +1,4 @@
-import {
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardTitle,
-} from "@ionic/react";
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from "@ionic/react";
 
 interface ResourceCardProps {
   title: string;
@@ -12,26 +7,23 @@ interface ResourceCardProps {
   action: { text: string; link: string };
 }
 
-const ResourceCard = ({
-  title,
-  description,
-  image,
-  action,
-}: ResourceCardProps) => {
+const ResourceCard = ({ title, description, image, action }: ResourceCardProps) => {
   return (
     <IonCard color="warning">
-      <img src={image} alt="CYCM Circle" />
+      <img src={image} alt="CYCM Circle" className="mx-auto mt-6" width={250} />
       <IonCardHeader className="text-center">
         <IonCardTitle className="text-2xl">{title}</IonCardTitle>
       </IonCardHeader>
       <IonCardContent className="text-center">
-        <p className="text-md font-sans my-2">{description}</p>
-        <button
-          type="button"
-          className="mt-auto text-gray-900 hover:text-white bg-gray-200 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-md px-5 py-2 text-center my-2"
-        >
-          {action.text}
-        </button>
+        <div style={{ height: "100%" }}>
+          <p className="text-md font-sans h-12">{description}</p>
+          <button
+            type="button"
+            className="text-gray-900 hover:text-white bg-gray-200 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-md px-5 py-2 text-center my-2"
+          >
+            {action.text}
+          </button>
+        </div>
       </IonCardContent>
     </IonCard>
   );
