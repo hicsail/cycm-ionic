c0013c22014b6b78c020f15f2f471dc01ca8a409
Asad <am5815@bu.edu>
2023-12-13 10:34:42 -0500

    prettier fix

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -21,9 +21,14 @@ const ResourceCard = ({
   color,
 }: ResourceCardProps) => {
   return (
-    <IonCard color="tertiary" >
-      <div style={{height: 120}}>
-      <img src={image} alt="CYCM Circle" className="mx-auto mt-6" width={250} />
+    <IonCard color="tertiary">
+      <div style={{ height: 120 }}>
+        <img
+          src={image}
+          alt="CYCM Circle"
+          className="mx-auto mt-6"
+          width={250}
+        />
       </div>
       <IonCardHeader className="text-center">
         <IonCardTitle className="text-2xl">{title}</IonCardTitle>
@@ -33,8 +38,18 @@ const ResourceCard = ({
           <p className="text-md font-sans h-12">{description}</p>
         </div>
       </IonCardContent>
-      <div className="relative" style={{ width: '100%', textAlign: 'center', height: 100}}>
-        <div style={{ position: 'absolute', bottom: 20, left: '50%', transform: 'translateX(-50%)' }}>
+      <div
+        className="relative"
+        style={{ width: "100%", textAlign: "center", height: 100 }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            bottom: 20,
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
           <button
             type="button"
             className="text-gray-900 hover:text-white bg-gray-200 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-md px-5 py-2 text-center my-2"
