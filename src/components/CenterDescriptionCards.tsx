d58cc2f6fee90fc6ff6da837f889185bee9de312
wenhwang97 <wenhwang@bu.edu>
2024-03-29 16:34:34 -0400

    adjust center card row display

--- src/components/CenterDescriptionCards.tsx
+++ src/components/CenterDescriptionCards.tsx
@@ -2,13 +2,11 @@ import CenterDescriptionCard from "./CenterDescriptionCard";
 
 export default function CenterDescriptionCards() {
   return (
-    <div>
-      <div className="flex flex-col items-center mt-8">
-        <div className="flex gap-8 justify-center max-w-7xl px-8 flex-col md:px-12 md:flex-row z-[1]">
-          <CenterDescriptionCard description="Create educational content approved by mental health licensed clinicians and teens" />
-          <CenterDescriptionCard description="Build community partnerships to support youth through mental health programs and training" />
-          <CenterDescriptionCard description="Partner with researchers to design youth mental health programs targetting needs of diverse youth" />
-        </div>
+    <div className="flex flex-col max-w-7xl items-center mt-8 mx-auto">
+      <div className="flex gap-4 px-8 flex-col lg:flex-row z-[1]">
+        <CenterDescriptionCard description="Create educational content approved by mental health licensed clinicians and teens" />
+        <CenterDescriptionCard description="Build community partnerships to support youth through mental health programs and training" />
+        <CenterDescriptionCard description="Partner with researchers to design youth mental health programs targetting needs of diverse youth" />
       </div>
     </div>
   );
