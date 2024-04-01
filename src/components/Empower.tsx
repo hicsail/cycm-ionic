85abb04e913ac52632f32f50fc2ecef7fc635b65
wenhwang97 <wenhwang@bu.edu>
2024-04-01 08:41:56 -0400

    change text color to white

--- src/components/Empower.tsx
+++ src/components/Empower.tsx
@@ -14,7 +14,7 @@ const Empower = ({ title, description, bgColor, btnElement }: EmpowerProps) => {
       >
         <div className="flex flex-col items-start justify-center my-4">
           <h1 className="text-4xl font-bold font-sans md:text-4xl mb-4">{title}</h1>
-          <p className="text-md font-sans font-semibold text-center md:text-lg mb-4">{description}</p>
+          <p className="text-md font-sans font-semibold text-center md:text-lg mb-4 text-white">{description}</p>
           <div className="flex mx-auto">{btnElement}</div>
         </div>
       </div>
--- src/components/SocialInfographics.tsx
+++ src/components/SocialInfographics.tsx
@@ -17,15 +17,15 @@ export default function SocialInfographics() {
         {/* Assuming these divs contain the other two graphics, adjust as necessary */}
         <div className="w-full [font-family:'Montserrat',Helvetica] font-bold">
           <img src="/clock.svg" alt="Graphic 1" className="md:w-1/2 h-auto mb-4" />
-          <h1 className="text-6xl md:text-[72px] lg:text-[100px] leading-none">3.5 hrs</h1>
-          <h4 className="text-2xl md:text-3xl lg:text-4xl leading-tight">
+          <h1 className="text-6xl md:text-[72px] lg:text-[100px] leading-none text-white">3.5 hrs</h1>
+          <h4 className="text-2xl md:text-3xl lg:text-4xl leading-tight text-white">
             is the average daily time spent on social media platforms by 8th and 10th graders.
           </h4>
         </div>
         <div className="w-full [font-family:'Montserrat',Helvetica] font-bold">
           <img src="/battery.svg" alt="Graphic 2" className="md:w-3/4 h-auto mb-4" />
-          <h1 className="text-6xl md:text-[72px] lg:text-[100px] leading-none">95%</h1>
-          <h4 className="text-2xl md:text-3xl lg:text-2xl leading-tight">
+          <h1 className="text-6xl md:text-[72px] lg:text-[100px] leading-none text-white">95%</h1>
+          <h4 className="text-2xl md:text-3xl lg:text-2xl leading-tight text-white">
             of teens ages 13 to 17 use social media, with more than 1 in 3 reporting they use it “almost constantly.”
           </h4>
         </div>
