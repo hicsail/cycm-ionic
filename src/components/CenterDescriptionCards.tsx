6013dc48b644da5222ea0da020bb092fad3925d2
wenhwang97 <wenhwang@bu.edu>
2024-04-17 08:55:42 -0400

    adjust sizing for cards

--- src/components/CenterDescriptionCards.tsx
+++ src/components/CenterDescriptionCards.tsx
@@ -2,12 +2,10 @@ import CenterDescriptionCard from "./CenterDescriptionCard";
 
 export default function CenterDescriptionCards() {
   return (
-    <div className="flex flex-col max-w-7xl items-center mt-8 mx-auto">
-      <div className="flex gap-4 px-8 flex-col lg:flex-row z-[1]">
-        <CenterDescriptionCard description="Create educational content approved by mental health licensed clinicians and teens" />
-        <CenterDescriptionCard description="Build community partnerships to support youth through mental health programs and training" />
-        <CenterDescriptionCard description="Partner with researchers to design youth mental health programs targetting needs of diverse youth" />
-      </div>
+    <div className="flex md:flex-row md:flex-wrap flex-col max-w-7xl items-center mt-8 mx-auto md:gap-4 px-8 z-[1] justify-center">
+      <CenterDescriptionCard description="Create educational content approved by mental health licensed clinicians and teens" />
+      <CenterDescriptionCard description="Build community partnerships to support youth through mental health programs and training" />
+      <CenterDescriptionCard description="Partner with researchers to design youth mental health programs targetting needs of diverse youth" />
     </div>
   );
 }
