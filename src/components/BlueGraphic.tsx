ec12178612290342f1daa1bb443675e9dbc8ef92
wenhwang97 <wenhwang@bu.edu>
2024-04-17 09:16:22 -0400

    center infographics text

--- src/components/BlueGraphic.tsx
+++ src/components/BlueGraphic.tsx
@@ -4,10 +4,8 @@ export default function () {
       <div className="w-full md:w-1/2 mb-4 md:mb-0">
         <img src="/blue-info.svg" className="w-2/3 md:w-auto mx-auto" />
       </div>
-      <div className="text-[#101066] font-semibold text-center md:text-left pl-16">
-        <h1 className="text-6xl md:text-[100px] leading-tight tracking-tighter mb-2 md:mb-0">
-          4 in 10
-        </h1>
+      <div className="text-[#101066] font-semibold text-center md:text-left md:pl-16">
+        <h1 className="text-6xl md:text-[100px] leading-tight tracking-tighter mb-2 md:mb-0">4 in 10</h1>
         <p className="text-xl md:text-[22px] leading-tight ">
           high school students felt
           <br />
