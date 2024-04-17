6825ca9a76a88bc6b23e819b4819544a46cdb0fc
wenhwang97 <wenhwang@bu.edu>
2024-04-17 09:28:03 -0400

    fix empower sizing

--- src/components/Empower.tsx
+++ src/components/Empower.tsx
@@ -7,21 +7,17 @@ interface EmpowerProps {
 
 const Empower = ({ title, description, bgColor, btnElement }: EmpowerProps) => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center p-8">
       <div
-        className="max-w-xl md:max-w-3xl md:px-20 md:py-8 px-8 py-4 "
+        className="max-w-xs sm:max-w-lg md:max-w-3xl md:px-20 md:py-8 px-8 py-4 "
         style={{
           backgroundColor: bgColor ? bgColor : "#fff",
           borderRadius: 40,
         }}
       >
         <div className="flex flex-col items-start justify-center my-4">
-          <h1 className="text-4xl font-bold font-sans md:text-4xl mb-4">
-            {title}
-          </h1>
-          <p className="text-md font-sans font-semibold text-center md:text-lg mb-4 text-white">
-            {description}
-          </p>
+          <h1 className="text-4xl font-bold font-sans md:text-4xl mb-4">{title}</h1>
+          <p className="text-md font-sans font-semibold text-center md:text-lg mb-4 text-white">{description}</p>
           <div className="flex mx-auto">{btnElement}</div>
         </div>
       </div>
