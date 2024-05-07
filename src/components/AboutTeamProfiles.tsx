55634c05f9c3a96b9f95e9df6ff29cbf70196cd5
Asad <am5815@bu.edu>
2024-05-07 10:33:46 -0400

    code to fetch profiles from strapi

--- src/components/AboutTeamProfiles.tsx
+++ src/components/AboutTeamProfiles.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Profile from "./Profile";
 const profiles = [
   {
@@ -66,7 +66,26 @@ const profiles = [
   },
 ];
 
+const token = import.meta.env.VITE_STRAPY_TOKEN;
+
 export default function AboutTeamProfiles() {
+
+  useEffect(() => {
+    // fetch from localhost:1337/api/articles
+    fetch(`${import.meta.env.VITE_STRAPI_URL}/api/people?populate=*`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((resp) => {
+        console.log(resp);
+      });
+  }, []);
+
+  
   return (
     <div>
       <div className="flex flex-col justify-center items-center min-h-screen px-12 md:px-0">
