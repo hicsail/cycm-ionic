4db1202d97c798f2b398b026869ce2978e6161b9
Asad <am5815@bu.edu>
2024-06-10 13:55:17 -0400

    prettier fix

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,7 +12,7 @@ const Index: React.FC = () => {
     <div>
       <NewHero />
       <div className="bg-gradient-to-b from-[#FFE2E2] to-[#FFFFFF]">
-        <div className="h-12 md:h-64"/>
+        <div className="h-12 md:h-64" />
         <CenterDescriptionCards />
         <div className="relative max-w-5xl mx-auto my-16">
           <img className="w-full h-auto" alt="Vector" src="vector.svg" />
