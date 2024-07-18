ad854b96b3bd5c5d0acc59f16445dbdd9418744f
QJWork <qjin21@bu.edu>
2024-07-18 15:31:13 -0400

    feat: futureVision

--- src/components/AboutFutureVisionSections.tsx
+++ src/components/AboutFutureVisionSections.tsx
@@ -6,42 +6,35 @@ import {
 } from "@ionic/react";
 import React from "react";
 
-
 export default function AboutFutureVisionSections() {
   return (
-    <div className="flex flex-col items-center justify-center p-5 ] bg-[#FFE2E2] min-h-screen box-border text-center text-base text-[#101066]">
+    <div className="flex flex-col items-center justify-center p-5 bg-[#FFE2E2] min-h-screen box-border text-center text-base text-[#101066]">
       <div className="text-4xl font-bold mb-8">
         <h1>FUTURE VISION</h1>
       </div>
-      <div className="flex flex-col gap-5 w-4/5 box-border md:flex-row">
-        <IonCard className="flex-1 bg-white text-[#100f66] rounded-lg p-5 shadow-md mb-5">
-            <IonCardTitle className="text-[#100f66] font-bold">COLLABORATIVE CREATION</IonCardTitle>
-          <IonCardContent>
-            <ul>
-              <li>Opportunities for trainees in all disciplines to collaborate</li>
-              <li>Create content for website and social media</li>
-            </ul>
+      <div className="container sm:px-2 md:px-12 px-36 mx-auto flex flex-col gap-5 w-19/20 box-border md:flex-row">
+        <IonCard className="flex-1 bg-white text-[#100f66] rounded-lg shadow-md pb-5 pl-0 pr-10 mb-0 ml-0">
+          <IonCardHeader className="text-left">
+            <IonCardTitle className="text-[#100f66] font-bold text-xl mb-4">COLLABORATIVE CREATION</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent className="text-left">
+            Create opportunities for trainees in all disciplines to collaborate and develop content for the website and social media.
           </IonCardContent>
         </IonCard>
-        <IonCard className="flex-1 bg-white text-[#100f66] rounded-lg p-5 shadow-md mb-5">
-          <IonCardHeader >
-            <IonCardTitle className="text-[#100f66] font-bold">COMMUNITY ENGAGEMENT</IonCardTitle>
+        <IonCard className="flex-1 bg-white text-[#100f66] rounded-lg shadow-md pb-5 pl-0 pr-10 mb-0 ml-0">
+          <IonCardHeader className="text-left">
+            <IonCardTitle className="text-[#100f66] font-bold text-xl mb-4">COMMUNITY ENGAGEMENT</IonCardTitle>
           </IonCardHeader>
-          <IonCardContent>
-            <ul>
-              <li>Design training toolkits to be used at schools and community-based organizations</li>
-              <li>Engage with youth outside of the clinical setting and in the community</li>
-            </ul>
+          <IonCardContent className="text-left">
+            Design training toolkits to be used at schools and community-based organizations to engage with youth outside of the clinical setting and in the community.
           </IonCardContent>
         </IonCard>
-        <IonCard className="flex-1 bg-white text-[#100f66] rounded-lg p-5 shadow-md mb-5">
-          <IonCardHeader>
-            <IonCardTitle className="text-[#100f66] font-bold">NATIONAL LEADERSHIP</IonCardTitle>
+        <IonCard className="flex-1 bg-white text-[#100f66] rounded-lg shadow-md pb-5 pl-0 pr-10 mb-0 ml-0">
+          <IonCardHeader className="text-left">
+            <IonCardTitle className="text-[#100f66] font-bold text-xl mb-4">NATIONAL LEADERSHIP</IonCardTitle>
           </IonCardHeader>
-          <IonCardContent>
-            <ul>
-              <li>BMC Department of Psychiatry: National "go-to" resource for mental health in diverse teens</li>
-            </ul>
+          <IonCardContent className="text-left">
+            BMC Department of Psychiatry: National "go-to" resource for mental health in diverse teens.
           </IonCardContent>
         </IonCard>
       </div>
