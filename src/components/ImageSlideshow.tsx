011e6d493b48cd994327cece514a7149f0b7725d
Kendree Chen <kendree.chen@gmail.com>
2024-07-25 13:49:52 -0400

    style slideshow

--- src/components/ImageSlideshow.tsx
+++ src/components/ImageSlideshow.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   IonButton,
   IonCard,
@@ -8,38 +8,67 @@ import {
 } from "@ionic/react";
 import { arrowForwardOutline } from "ionicons/icons";
 
+type SlideComponent = {
+  background: string;
+  description: string;
+};
+
+const token = import.meta.env.VITE_STRAPY_TOKEN;
+
 export default function ImageSlideshow() {
   const [slide, setSlide] = React.useState(0);
-  const descriptions = [
-    "Create educational content approved by mental health licensed clinicians and teens",
-    "Build community partnerships to support youth through mental health programs and training",
-    "Partner with researchers to design youth mental health programs targetting needs of diverse youth",
-  ];
-  const backgroundtemp = ["public/TempFunctionImage.png", "", ""];
+  const [slides, setSlides] = React.useState<SlideComponent[]>([]);
+  
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_STRAPI_URL}/api/slides?populate=*`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((resp) => {
+        const data = resp.data;
+        console.log(data)
+        let slides: SlideComponent[] = data.map((slide: any) => {
+          console.log(slide)
+          console.log(slide.attributes.background.data[0].attributes.name)
+          return {
+            background: slide.attributes.background.data[0].attributes.name,
+            description: slide.attributes.description,
+          };
+        });
+        setSlides(slides);
+      });
+    }, []);
+
   const nextSlide = () => {
-    setSlide(slide > descriptions.length - 2 ? 0 : slide + 1);
+    setSlide(slide > slides.length - 2 ? 0 : slide + 1);
   };
 
   return (
     <div className="mt-8 mx-auto w-3/4 px-8 justify-center">
       <div>
         <IonCard
-          className={`flex flex-col items-center w-auto h-96 object-stretch bg-[url('${backgroundtemp[slide]}')] mx-auto `}
+          className={`flex flex-col items-center w-auto h-72 sm:h-96 md:h-[28rem] lg:h-[32rem] xl:h-[36rem] object-stretch mx-auto justify-center overflow-hidden`}
         >
-          <IonCardContent className="align-middle py-8 px-6">
-            <IonCardSubtitle
+          <img src = {slides[slide].background} alt="slide" className="object-cover shrink-0 min-w-full min-h-[115%]" />
+            <IonCardSubtitle className = "md:text-lg lg:text-2xl xl:text-3xl text-md w-full md:w-3/4"
               style={{
-                fontSize: 26,
                 color: "white",
                 fontWeight: "bold",
                 textAlign: "center",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
               }}
             >
-              {descriptions[slide]}
+              {slides[slide].description}
             </IonCardSubtitle>
-          </IonCardContent>
           <IonButton
-            className="absolute top-[83%] right-[0.5rem] justify-end w-[4rem] h-[3rem] "
+            className="absolute bottom-[0.5rem] right-[0.5rem] justify-end w-[4rem] h-[3rem] "
             onClick={nextSlide}
             color="light"
             size="small"
