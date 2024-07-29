33fb5fb270469ec36dfb24185ab271b3f0f4541a
QJWork <qjin21@bu.edu>
2024-07-29 10:39:58 -0400

    feat: fix image length <0 problem

--- src/components/ImageSlideshow.tsx
+++ src/components/ImageSlideshow.tsx
@@ -53,43 +53,47 @@ export default function ImageSlideshow() {
         <IonCard
           className={`flex flex-col items-center w-auto h-72 sm:h-96 md:h-[28rem] lg:h-[32rem] xl:h-[36rem] object-stretch mx-auto justify-center overflow-hidden`}
         >
-          <img
-            src={slides[slide].background}
-            alt="slide"
-            className="object-cover shrink-0 min-w-full min-h-[115%]"
-          />
-          <IonCardSubtitle
-            className="md:text-lg lg:text-2xl xl:text-3xl text-md w-full md:w-3/4"
-            style={{
-              color: "white",
-              fontWeight: "bold",
-              textAlign: "center",
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-            }}
-          >
-            {slides[slide].description}
-          </IonCardSubtitle>
-          <IonButton
-            className="absolute bottom-[0.5rem] right-[0.5rem] justify-end w-[4rem] h-[3rem] "
-            onClick={nextSlide}
-            color="light"
-            size="small"
-            shape="round"
-            style={{
-              "--box-shadow": "none",
-              "--ripple-color": "transparent",
-              transition: "width 2s, height 4s",
-            }}
-          >
-            <IonIcon
-              size="large"
-              icon={arrowForwardOutline}
-              style={{ color: "red" }}
-            />
-          </IonButton>
+          {slides.length > 0 && (
+            <>
+              <img
+                src={slides[slide].background}
+                alt="slide"
+                className="object-cover shrink-0 min-w-full min-h-[115%]"
+              />
+              <IonCardSubtitle
+                className="md:text-lg lg:text-2xl xl:text-3xl text-md w-full md:w-3/4"
+                style={{
+                  color: "white",
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                }}
+              >
+                {slides[slide].description}
+              </IonCardSubtitle>
+              <IonButton
+                className="absolute bottom-[0.5rem] right-[0.5rem] justify-end w-[4rem] h-[3rem] "
+                onClick={nextSlide}
+                color="light"
+                size="small"
+                shape="round"
+                style={{
+                  "--box-shadow": "none",
+                  "--ripple-color": "transparent",
+                  transition: "width 2s, height 4s",
+                }}
+              >
+                <IonIcon
+                  size="large"
+                  icon={arrowForwardOutline}
+                  style={{ color: "red" }}
+                />
+              </IonButton>
+            </>
+          )}
         </IonCard>
       </div>
     </div>
